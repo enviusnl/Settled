763194ef1dd8b241dee5c5db735da49d55a8ef02
Koen Vendrik <k.vendrik@gmail.com>
2014-12-04 14:33:19 +0100

    Removes Mac OS X from welcome message because its not tested yet

--- settled.bash
+++ settled.bash
@@ -46,7 +46,7 @@ and we'll look into it."
 fi
 
 
-printf "\033[1;32mSettled\033[0m will turn your Mac OS X or Ubuntu machine into an 
+printf "\033[1;32mSettled\033[0m will turn your Ubuntu 14.4 machine into an 
 \033[1;32mawesome front-end tool\033[0m.
 
 What it installs can be customized by editing this file.
@@ -58,8 +58,7 @@ Vim, Node.js, Heroku Toolbelt,
 Phonegap, Grunt-cli, Gulp, Bower, Yeoman,
 SASS, Compass.
 
-If you're on Linux we will prompt you to ask if you also would 
-like to install:
+On Linux we will prompt you to ask if you also would like to install:
 
 Apache2, MySQL, PHP, Sublime Text 3.
 
