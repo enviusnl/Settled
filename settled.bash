b47418a2feedea371652f50307753c1535870dc0
Koen Vendrik <k.vendrik@gmail.com>
2014-12-04 09:39:05 +0100

    Makes sure Homebrew is installed before continuing

--- settled.bash
+++ settled.bash
@@ -138,6 +138,12 @@ fancy_echo 32 "✔ Sass and Compass installed, Homebrew awesomeness coming up...
 
 # loop Homebrew packages
 # install whats not installed already
+if ! check_installed brew; then
+	fancy_echo 31 "✘ Homebrew was not installed. Make sure you run Settled without sudo."
+	exit 1
+fi
+
+brew doctor
 BREW_PKGS=("vim" "git" "node" "heroku-toolbelt")
 for name in "${BREW_PKGS[@]}"
 do
